f84aa9bf08d40d4c9a3af138467d90cd48839b5c
RogueProeliator <adam.d.ashe@gmail.com>
2023-01-05 22:45:18 -0600

    Python3 & RPFramework refactor updates

--- src/Niles Audio Receiver.indigoPlugin/Contents/Server Plugin/nilesAudioDevices.py
+++ src/Niles Audio Receiver.indigoPlugin/Contents/Server Plugin/nilesAudioDevices.py
@@ -1,31 +1,28 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
 # Niles Audio Receiver Plugin by RogueProeliator <rp@rogueproeliator.com>
 # 	See plugin.py for more plugin details and information
 #/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
 
 #/////////////////////////////////////////////////////////////////////////////////////////
-# Python imports
-#/////////////////////////////////////////////////////////////////////////////////////////
-import os
-import Queue
+#region Python imports
 import re
-import string
-import sys
-import threading
 
 import indigo
-import RPFramework
+import RPFramework.RPFrameworkTelnetDevice
 
+from RPFramework.RPFrameworkTelnetDevice import RPFrameworkTelnetDevice
+from RPFramework.RPFrameworkCommand import RPFrameworkCommand
+#endregion
+#/////////////////////////////////////////////////////////////////////////////////////////
 
 #/////////////////////////////////////////////////////////////////////////////////////////
-# Constants and configuration variables
-#/////////////////////////////////////////////////////////////////////////////////////////
+#region Constants and configuration variables
 CMD_CREATEZONESTATUSUPDATECOMMAND = "createZoneStatusUpdateCommands"
 CMD_ACTIVATEZONEFORCOMMAND        = "activateZoneForCommand"
+#endregion
+#/////////////////////////////////////////////////////////////////////////////////////////
 
 
 #/////////////////////////////////////////////////////////////////////////////////////////
@@ -33,7 +30,7 @@ CMD_ACTIVATEZONEFORCOMMAND        = "activateZoneForCommand"
 #	Handles the communications and status of a Niles audio receiver which is connected
 #	via the serial port
 #/////////////////////////////////////////////////////////////////////////////////////////
-class NilesAudioReceiverDevice(RPFramework.RPFrameworkTelnetDevice.RPFrameworkTelnetDevice):
+class NilesAudioReceiverDevice(RPFrameworkTelnetDevice):
 	
 	#/////////////////////////////////////////////////////////////////////////////////////
 	# Class construction and destruction methods
@@ -44,10 +41,9 @@ class NilesAudioReceiverDevice(RPFramework.RPFrameworkTelnetDevice.RPFrameworkTe
 	# member variables
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def __init__(self, plugin, device):
-		super().__init__(plugin, device, connectionType=RPFramework.RPFrameworkTelnetDevice.CONNECTIONTYPE_SERIAL)
-		self.activeControlZone = 0
-		
-		
+		super().__init__(plugin, device, connection_type=RPFrameworkTelnetDevice.CONNECTIONTYPE_SERIAL)
+		self.active_control_zone = 0
+
 	#/////////////////////////////////////////////////////////////////////////////////////
 	# Processing and command functions
 	#/////////////////////////////////////////////////////////////////////////////////////
@@ -58,29 +54,27 @@ class NilesAudioReceiverDevice(RPFramework.RPFrameworkTelnetDevice.RPFrameworkTe
 	def handleUnmanagedCommandInQueue(self, ipConnection, rpCommand):
 		if rpCommand.commandName == CMD_CREATEZONESTATUSUPDATECOMMAND:
 			# create a set of commands to update the status of a single zone
-			updateCommandList = []
-			updateCommandList.append(self.createZoneActivateCommand(rpCommand.commandPayload))
-			updateCommandList.append(self.createZoneStatusRequestCommand(rpCommand.commandPayload))
-			self.queueDeviceCommands(updateCommandList)
+			update_command_list = [self.createZoneActivateCommand(rpCommand.commandPayload), self.createZoneStatusRequestCommand(rpCommand.commandPayload)]
+			self.queueDeviceCommands(update_command_list)
 		
 		elif rpCommand.commandName == "createAllZonesStatusRequestCommands":
 			# create a set of commands to update the status of all zones defined by the
 			# plugin (as child devices)
-			updateCommandList = []
+			update_command_list = []
 			for zoneNumber in self.childDevices:
-				updateCommandList.append(self.createZoneActivateCommand(zoneNumber))
-				updateCommandList.append(self.createZoneStatusRequestCommand(zoneNumber))
+				update_command_list.append(self.createZoneActivateCommand(zoneNumber))
+				update_command_list.append(self.createZoneStatusRequestCommand(zoneNumber))
 			
-			# queue up all of the commands at once (so they will run back to back)
-			self.queueDeviceCommands(updateCommandList)
+			# queue up all the commands at once (so they will run back to back)
+			self.queueDeviceCommands(update_command_list)
 			
 		elif rpCommand.commandName == CMD_ACTIVATEZONEFORCOMMAND:
 			# this command will immediately activate the requested zone (per the payload)
 			# for control if it is not already active
-			if self.activeControlZone != int(rpCommand.commandPayload):
+			if self.active_control_zone != int(rpCommand.commandPayload):
 				self.hostPlugin.logger.threaddebug(f"Writing activate zone request for zone {rpCommand.commandPayload}")
-				writeCommand = f"znc,4,{rpCommand.commandPayload}\r"
-				ipConnection.write(writeCommand.encode("ascii"))
+				write_command = f"znc,4,{rpCommand.commandPayload}\r"
+				ipConnection.write(write_command.encode("ascii"))
 			else:
 				self.hostPlugin.logger.threaddebug(f"Zone {rpCommand.commandPayload} already active, ignoring activate zone command for efficiency")
 				
@@ -91,28 +85,27 @@ class NilesAudioReceiverDevice(RPFramework.RPFrameworkTelnetDevice.RPFrameworkTe
 		elif rpCommand.commandName == "createAllZonesMuteCommands":
 			# this command will be fired whenever the plugin needs to create the commands that will mute
 			# all zones (must be done individually)
-			muteCommandList = []
+			mute_command_list = []
 			for zoneNumber in self.childDevices:
 				if self.childDevices[zoneNumber].indigoDevice.states["isPoweredOn"] == True and self.childDevices[zoneNumber].indigoDevice.states["isMuted"] == False:
 					self.hostPlugin.logger.threaddebug(f"Mute All: muting zone {zoneNumber}")
-					muteCommandList.append(RPFramework.RPFrameworkCommand.RPFrameworkCommand(RPFramework.RPFrameworkTelnetDevice.CMD_WRITE_TO_DEVICE, commandPayload=f"zsc,{zoneNumber},11", postCommandPause=0.1))
-					muteCommandList.append(RPFramework.RPFrameworkCommand.RPFrameworkCommand(CMD_CREATEZONESTATUSUPDATECOMMAND, commandPayload=str(zoneNumber), postCommandPause=0.1))
-			self.queueDeviceCommands(muteCommandList)
+					mute_command_list.append(RPFrameworkCommand(RPFrameworkTelnetDevice.CMD_WRITE_TO_DEVICE, commandPayload=f"zsc,{zoneNumber},11", postCommandPause=0.1))
+					mute_command_list.append(RPFrameworkCommand(CMD_CREATEZONESTATUSUPDATECOMMAND, commandPayload=str(zoneNumber), postCommandPause=0.1))
+			self.queueDeviceCommands(mute_command_list)
 	
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This routine will be called in order to generate the commands necessary to update
 	# the status of a zone defined for this receiver
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def createZoneStatusRequestCommand(self, zoneNumber):
-		return RPFramework.RPFrameworkCommand.RPFrameworkCommand(RPFramework.RPFrameworkTelnetDevice.CMD_WRITE_TO_DEVICE, commandPayload="znc,5", postCommandPause=0.1)
+		return RPFrameworkCommand(RPFrameworkTelnetDevice.CMD_WRITE_TO_DEVICE, commandPayload="znc,5", postCommandPause=0.1)
 			
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This routine will be called in order to generate the commands necessary to activate
 	# a zone (for control) on this receiver
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def createZoneActivateCommand(self, zoneNumber):	
-		return RPFramework.RPFrameworkCommand.RPFrameworkCommand(CMD_ACTIVATEZONEFORCOMMAND, commandPayload=str(zoneNumber), postCommandPause=0.1)
-			
+		return RPFrameworkCommand(CMD_ACTIVATEZONEFORCOMMAND, commandPayload=str(zoneNumber), postCommandPause=0.1)
 
 	#/////////////////////////////////////////////////////////////////////////////////////
 	# Custom Response Handlers
@@ -124,64 +117,64 @@ class NilesAudioReceiverDevice(RPFramework.RPFrameworkTelnetDevice.RPFrameworkTe
 	def zoneStatusResponseReceived(self, responseObj, rpCommand):
 		# the response format is a comma-delimited list with the following values:
 		# USC, 2, [ZONE], [SOURCE #], [0|1 - ON/OFF], [VOLUME], [0|1 MUTE], [BASE], [TREB]
-		responseParser = re.compile(r'^usc,2,(?P<zone>\d+),(?P<source>\d+),(?P<onOff>0|1),(?P<volume>\d+),(?P<mute>0|1),(?P<base>\d+),(?P<treble>\d+)$', re.I)
-		statusObj = responseParser.match(responseObj)
-		statusInfo = statusObj.groupdict()
+		response_parser = re.compile(r'^usc,2,(?P<zone>\d+),(?P<source>\d+),(?P<onOff>0|1),(?P<volume>\d+),(?P<mute>0|1),(?P<base>\d+),(?P<treble>\d+)$', re.I)
+		status_obj = response_parser.match(responseObj)
+		status_info = status_obj.groupdict()
 		
 		# device status updates are expensive, so only do the update on statuses that are
 		# different than current
-		self.hostPlugin.logger.debug(f"Received status update for Zone {statusInfo['zone']}: {responseObj}")
-		zoneDevice = self.childDevices[statusInfo["zone"]]
+		self.hostPlugin.logger.debug(f"Received status update for Zone {status_info['zone']}: {responseObj}")
+		zone_device = self.childDevices[status_info["zone"]]
 
 		# get the on/off status as this will determine what info we update; do not update it now
 		# since our uiValue may change depending upon other conditions
-		statusIsPoweredOn = statusInfo["onOff"] == "1"
-		forceUIValueUpdate = False
-		onOffUIValue = ""
+		status_is_powered_on = status_info["onOff"] == "1"
+		force_ui_value_update = False
+		on_off_ui_value = ""
 
 		# we may only update the remainder of the states if the zone is powered on... otherwise
 		# the information is not reliable
-		if statusIsPoweredOn == True:
-			zoneStatesToUpdate = []
+		if status_is_powered_on:
+			zone_states_to_update = []
 		
-			if zoneDevice.indigoDevice.states.get("source", "") != statusInfo["source"]:
-				uiSourceValue = self.indigoDevice.pluginProps.get(f"source {statusInfo['source']} Label", "")
-				if uiSourceValue == "":
-					uiSourceValue = statusInfo["source"]
-				zoneStatesToUpdate.append({ "key" : "source", "value" : int(statusInfo["source"]), "uiValue" : uiSourceValue})
+			if zone_device.indigoDevice.states.get("source", "") != status_info["source"]:
+				ui_source_value = self.indigoDevice.pluginProps.get(f"source {status_info['source']} Label", "")
+				if ui_source_value == "":
+					ui_source_value = status_info["source"]
+				zone_states_to_update.append({"key": "source", "value": int(status_info["source"]), "uiValue": ui_source_value})
 		
-			statusVolume = int(statusInfo["volume"])
-			if int(zoneDevice.indigoDevice.states.get("volume", "0")) != statusVolume:
-				forceUIValueUpdate = True
-				zoneStatesToUpdate.append({ "key" : "volume", "value" : statusVolume})
+			status_volume = int(status_info["volume"])
+			if int(zone_device.indigoDevice.states.get("volume", "0")) != status_volume:
+				force_ui_value_update = True
+				zone_states_to_update.append({"key": "volume", "value": status_volume})
 			
-			if zoneDevice.indigoDevice.states.get("isMuted", False) != (statusInfo["mute"] == "1"):
-				forceUIValueUpdate = True
-				zoneStatesToUpdate.append({ "key" : "isMuted", "value" : (statusInfo["mute"] == "1")})
+			if zone_device.indigoDevice.states.get("isMuted", False) != (status_info["mute"] == "1"):
+				force_ui_value_update = True
+				zone_states_to_update.append({"key": "isMuted", "value": (status_info["mute"] == "1")})
 			
-			statusBaseLevel = int(statusInfo["base"])
-			if int(zoneDevice.indigoDevice.states.get("baseLevel", "0")) != statusBaseLevel:
-				zoneStatesToUpdate.append({ "key" : "baseLevel", "value" : statusBaseLevel})
+			status_base_level = int(status_info["base"])
+			if int(zone_device.indigoDevice.states.get("baseLevel", "0")) != status_base_level:
+				zone_states_to_update.append({"key": "baseLevel", "value": status_base_level})
 			
-			statusTrebleLevel = int(statusInfo["treble"])
-			if int(zoneDevice.indigoDevice.states.get("trebleLevel", "0")) != statusTrebleLevel:
-				zoneStatesToUpdate.append({ "key" : "trebleLevel", "value" : statusTrebleLevel})
+			status_treble_level = int(status_info["treble"])
+			if int(zone_device.indigoDevice.states.get("trebleLevel", "0")) != status_treble_level:
+				zone_states_to_update.append({"key": "trebleLevel", "value": status_treble_level})
 				
 			# determine the on/off display text
-			if statusInfo["mute"] == "1" or statusVolume == 0:
-				onOffUIValue = "muted"
+			if status_info["mute"] == "1" or status_volume == 0:
+				on_off_ui_value = "muted"
 			else:
-				onOffUIValue = str(statusVolume)
+				on_off_ui_value = str(status_volume)
 				
-			if len(zoneStatesToUpdate) > 0:
-				zoneDevice.indigoDevice.updateStatesOnServer(zoneStatesToUpdate)
+			if len(zone_states_to_update) > 0:
+				zone_device.indigoDevice.updateStatesOnServer(zone_states_to_update)
 		else:
-			onOffUIValue = "off"
+			on_off_ui_value = "off"
 			self.hostPlugin.logger.debug("Skipping status update for zone that is off")
 			
 		# finally update the on/off state...
-		if zoneDevice.indigoDevice.states.get("isPoweredOn", False) != statusIsPoweredOn or forceUIValueUpdate == True:
-			zoneDevice.indigoDevice.updateStateOnServer(key="isPoweredOn", value=statusIsPoweredOn, uiValue=onOffUIValue)
+		if zone_device.indigoDevice.states.get("isPoweredOn", False) != status_is_powered_on or force_ui_value_update:
+			zone_device.indigoDevice.updateStateOnServer(key="isPoweredOn", value=status_is_powered_on, uiValue=on_off_ui_value)
 			
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This callback is made whenever the plugin has received the response to a request
@@ -189,10 +182,10 @@ class NilesAudioReceiverDevice(RPFramework.RPFrameworkTelnetDevice.RPFrameworkTe
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def activeControlZoneUpdated(self, responseObj, rpCommand):
 		# the response format is a comma-delimited response: rznc,4,[zone]
-		responseParser = re.compile(r'^rznc,4,(\d+)\s*$', re.I)
-		matchObj = responseParser.match(responseObj)
-		self.activeControlZone = int(matchObj.group(1))
-		self.hostPlugin.logger.threaddebug(f"Updated active control zone to {matchObj.group(1)}")
+		response_parser = re.compile(r'^rznc,4,(\d+)\s*$', re.I)
+		match_obj = response_parser.match(responseObj)
+		self.active_control_zone = int(match_obj.group(1))
+		self.hostPlugin.logger.threaddebug(f"Updated active control zone to {match_obj.group(1)}")
 				
 		
 #/////////////////////////////////////////////////////////////////////////////////////////
@@ -213,7 +206,6 @@ class NilesAudioZone(RPFramework.RPFrameworkNonCommChildDevice.RPFrameworkNonCom
 	def __init__(self, plugin, device):
 		super().__init__(plugin, device)
 		
-		
 	#/////////////////////////////////////////////////////////////////////////////////////
 	# Validation and GUI functions
 	#/////////////////////////////////////////////////////////////////////////////////////
@@ -224,14 +216,12 @@ class NilesAudioZone(RPFramework.RPFrameworkNonCommChildDevice.RPFrameworkNonCom
 	def getConfigDialogMenuItems(self, filter, valuesDict, typeId, targetId):
 		# we need the parent (receiver) device in order to get the list of
 		# available sources...
-		parentReceiver = self.hostPlugin.managedDevices[int(self.indigoDevice.pluginProps["sourceReceiver"])]
+		parent_receiver = self.hostPlugin.managed_devices[int(self.indigoDevice.pluginProps["sourceReceiver"])]
 		
-		sourceOptions = []
-		for x in range(1,7):
-			sourcePropName = "source" + RPFramework.RPFrameworkUtils.to_unicode(x) + "Label"
-			if parentReceiver.indigoDevice.pluginProps[sourcePropName] != "":
-				sourceOptions.append((RPFramework.RPFrameworkUtils.to_unicode(x), f"Source {x}: {parentReceiver.indigoDevice.pluginProps[sourcePropName]}"))
+		source_options = []
+		for x in range(1, 7):
+			source_prop_name = f"source{x}Label"
+			if parent_receiver.indigoDevice.pluginProps[source_prop_name] != "":
+				source_options.append((f"{x}", f"Source {x}: {parent_receiver.indigoDevice.pluginProps[source_prop_name]}"))
 			
-		return sourceOptions
-		
-	
+		return source_options
--- src/Niles Audio Receiver.indigoPlugin/Contents/Server Plugin/plugin.py
+++ src/Niles Audio Receiver.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -7,23 +7,15 @@
 #	
 #	Command structure based on Niles Audio's published specification found in the ZR-6
 #	instruction manual
-#
 #/////////////////////////////////////////////////////////////////////////////////////////
 
-
 #/////////////////////////////////////////////////////////////////////////////////////////
-# Python imports
-#/////////////////////////////////////////////////////////////////////////////////////////
-import re
-import string
-import os
+#region Python imports
 
-import RPFramework
 import nilesAudioDevices
+from RPFramework.RPFrameworkPlugin import RPFrameworkPlugin
 
-
-#/////////////////////////////////////////////////////////////////////////////////////////
-# Constants and configuration variables
+#endregion
 #/////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -32,13 +24,13 @@ import nilesAudioDevices
 #	Primary Indigo plugin class that is universal for all receiver devices to be
 #	controlled (this represents the master receiver)
 #/////////////////////////////////////////////////////////////////////////////////////////
-class Plugin(RPFramework.RPFrameworkPlugin.RPFrameworkPlugin):
+class Plugin(RPFrameworkPlugin):
 	
 	#/////////////////////////////////////////////////////////////////////////////////////
 	# Class construction and destruction methods
 	#/////////////////////////////////////////////////////////////////////////////////////
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Constructor called once upon plugin class creation; setup the device tracking
+	# Constructor called once upon plugin class creation; set up the device tracking
 	# variables for later use
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
@@ -49,27 +41,27 @@ class Plugin(RPFramework.RPFrameworkPlugin.RPFrameworkPlugin):
 	# This routine will be called from the user executing the menu item action to send
 	# an arbitrary command code to the Niles Audio receiver
 	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-	
-	def sendArbitraryCommand(self, valuesDict, typeId):
+	def send_arbitrary_command(self, valuesDict, typeId):
 		try:
-			deviceId = valuesDict.get("targetDevice", "0")
-			commandCode = valuesDict.get("commandToSend", "").strip()
+			device_id    = valuesDict.get("targetDevice", "0")
+			command_code = valuesDict.get("commandToSend", "").strip()
 		
-			if deviceId == "" or deviceId == "0":
+			if device_id == "" or device_id == "0":
 				# no device was selected
-				errorDict = indigo.Dict()
-				errorDict["targetDevice"] = "Please select a device"
-				return (False, valuesDict, errorDict)
-			elif commandCode == u'':
-				errorDict = indigo.Dict()
-				errorDict["commandToSend"] = "Enter command to send"
-				return (False, valuesDict, errorDict)
+				error_dict = indigo.Dict()
+				error_dict["targetDevice"] = "Please select a device"
+				return False, valuesDict, error_dict
+			elif command_code == "":
+				error_dict = indigo.Dict()
+				error_dict["commandToSend"] = "Enter command to send"
+				return False, valuesDict, error_dict
 			else:
 				# send the code using the normal action processing...
-				actionParams = indigo.Dict()
-				actionParams["commandCode"] = commandCode
-				self.executeAction(pluginAction=None, indigoActionId="SendArbitraryCommand", indigoDeviceId=int(deviceId), paramValues=actionParams)
-				return (True, valuesDict)
+				action_params = indigo.Dict()
+				action_params["commandCode"] = command_code
+				self.execute_action(pluginAction=None, indigoActionId="SendArbitraryCommand", indigoDeviceId=int(device_id), paramValues=action_params)
+				return True, valuesDict
 		except:
-			self.exceptionLog()
-			return (False, valuesDict)
+			self.logger.exception("Failed to send command to device")
+			return False, valuesDict
 			
